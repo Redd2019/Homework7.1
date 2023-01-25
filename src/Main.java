77d719e615a06bd397e899cf3c38b37e0ea4e8a3
Redd2019` <anton-mikerin@yandex.ru>
2023-01-25 11:33:45 +0300

    Выполнены задания 1 и 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
 
     public static void task1() {
+        System.out.println("Задание 1");
         int finalSum = 0;
         int numberOfMonths = 1;
         while (finalSum < 2459000) {
@@ -12,6 +14,20 @@ public class Main {
             numberOfMonths++;
             System.out.println("Месяц " + numberOfMonths + ", сумма накоплений равна " + finalSum + " рублей.");
         }
+        }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+           int a = 1;
+           for (;a <= 10;a++) {
+               System.out.print(a + " ");
+           }
+        System.out.println();
+           int b = 11;
+           while (b>1){
+               b--;
+               System.out.print(b + " ");
+           }
         }
 
     }
